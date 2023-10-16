6f97d70df3a7a3c3d589361a78ac707d7625b353
seomac <icyveins@hotmail.com>
2023-10-16 19:53:28 +0800

    complete most of the parse_header
    need to verify the final offset with the specified numBytes (if it is specified)

--- jagp/_parser.py
+++ jagp/_parser.py
@@ -38,14 +38,15 @@ sizeInference = {
 def parse(desc: dict, verbose: bool=True) -> dict:
     parsed = desc
     for header in parsed['headers']:
-        parsedfields = parse_header(header, verbose)
+        parsedheader, offset = parse_header(header, verbose)
+        print(parsedheader)
 
 
 def parse_header(header: dict, verbose: bool=True) -> dict:
     parsed = header
     # Iterate over the fields
-    parsedfields = list()
-    for field in parsed['fields']:
+    offset = 0 # Accumulated offset in bits
+    for i, field in enumerate(parsed['fields']):
         # Check if it's the shortcut string
         if isinstance(field, str):
             # Split into the name and the type
@@ -56,15 +57,21 @@ def parse_header(header: dict, verbose: bool=True) -> dict:
                     name, stdintDict[typekey], size
                 ))
 
-            # Change the field in-place?
+            # Infer offsets
+            byte_offset = offset // 8
+            bit_offset = offset % 8
 
-
-            # Push into a new list
-            parsedfields.append({
+            # Change the field in-place
+            parsed['fields'][i] = {
                 'name': name,
                 'type': stdintDict[typekey],
-                'size': size
-            })
+                'size': size,
+                'byte_offset': byte_offset,
+                'bit_offset': bit_offset
+            }
+
+            # Increment offset counter
+            offset += size
             
         # Otherwise if it's already a dict
         elif isinstance(field, dict):
@@ -76,25 +83,64 @@ def parse_header(header: dict, verbose: bool=True) -> dict:
 
             # Check if size was specified, otherwise infer it
             if field.get('size') is None:
-                field['size'] = sizeInference[field['type']] # Change in place
-            
-            # Push into a new list
-            parsedfields.append(field)
+                parsed['fields'][i]['size'] = sizeInference[field['type']] # Change in place
+                if verbose:
+                    print("Field '%s' size inferred to be %d bits" % (field['name'], parsed['fields'][i]['size']))
 
+            # Check if offsets were specified, infer them if they aren't
+            if field.get('byte_offset') is None:
+                byte_offset = offset // 8
+                if verbose:
+                    print("Field '%s' byte offset inferred to be %d" % (field['name'], byte_offset))
+            else:
+                byte_offset = field.get('byte_offset')
+
+            if field.get('bit_offset') is None:
+                bit_offset = offset % 8
+                if verbose:
+                    print("Field '%s' bit offset inferred to be %d" % (field['name'], bit_offset))
+            else:
+                bit_offset = field.get('bit_offset')
+
+            # Verify that the final offset values are valid
+            if byte_offset*8 + bit_offset < offset:
+                raise ValueError(
+                    "Field '%s' byte offset (%d) + bit offset (%d) is greater than the current offset (%d)" % (
+                    field['name'], byte_offset, bit_offset, offset)
+                )
+            elif byte_offset*8 + bit_offset != offset:
+                if verbose:
+                    print(
+                        "Padding detected before field '%s': byte offset (%d) + bit offset (%d) is greater than expected" % (
+                        field['name'], byte_offset, byte_offset)
+                    )
+                # Move our internal offset to the specified value
+                offset = byte_offset*8 + bit_offset
+            else:
+                # Otherwise increment the offset
+                offset += parsed['fields'][i]['size']
+            
+            # Write the offset values
+            parsed['fields'][i]['byte_offset'] = byte_offset
+            parsed['fields'][i]['bit_offset'] = bit_offset
+                
         else: 
             raise TypeError("Field must be just a string or a dict")
+        
+    # TODO: check if offset and optionally specified numBytes are valid
 
-    return parsedfields
+    return parsed, offset
             
 
 #%% Simple test of a parser
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", type=str)
+    parser.add_argument("-v", "--verbose", action="store_true", default=False)
     args = parser.parse_args()
 
     with open(args.input_file, "r") as fid:
         desc = load(fid, Loader=Loader)
 
-    print(desc)
+    print(parse(desc, args.verbose))
 
